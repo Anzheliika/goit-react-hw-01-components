2283cd4b7e3d23ae5ad315fdc499465ef1d0496d
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-05-16 18:21:45 +0300

    did second task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile/Profile';
+import { StatisticsList } from './StatisticsList/StatisticsList';
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
   return (
@@ -13,6 +15,8 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+
+      <StatisticsList items={data} />
     </>
   );
 };
---
+++ src/components/Statistic/Statistic.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import css from './Statistic.module.css';
+
+export const Statistic = ({ id, label, percentage }) => {
+  return (
+    <li key={id} className={css.item}>
+      <span className={css.label}>{label}</span>
+      <span className={css.percentage}>{percentage}%</span>
+    </li>
+  );
+};
+
+Statistic.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
